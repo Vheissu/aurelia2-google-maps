fa8667316242298ad53c430c2c48da7f35441573
Dwayne <dwaynecharrington@gmail.com>
2021-10-19 11:43:56 +1000

    feat(build): updating build process scripts and config

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,4 @@
-import typescript from 'rollup-plugin-typescript2';
+import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
 import { terser } from 'rollup-plugin-terser';
 import postcss from 'rollup-plugin-postcss';
@@ -6,24 +6,60 @@ import autoprefixer from 'autoprefixer';
 import cssnano from 'cssnano';
 import postcssUrl from 'postcss-url';
 import html from "rollup-plugin-html";
+import replace from '@rollup/plugin-replace';
 // import copy from 'rollup-plugin-copy';
+
+const tsPluginConfig = typescript({
+    tsconfig: 'tsconfig.json',
+    sourceMap: true,
+    include: ['src/**/*.ts'],
+    noEmitOnError: false,
+});
+
+const replacePluginCfg = replace({
+    values: {
+      __DEV__: String(false)
+    },
+    preventAssignment: true,
+});
+
+const terserPluginCfg = terser({
+    compress: {
+      defaults: false,
+    },
+    mangle: {
+      properties: {
+        regex: /^_/
+      }
+    },
+    format: {
+      beautify: true,
+    },
+    keep_classnames: true,
+});
 
 export default {
     input: 'src/index.ts',
-    output: [{
-            file: pkg.main,
-            format: 'cjs'
+    output: [
+        {
+          file: `dist/index.esm.js`,
+          format: 'es',
+          sourcemap: true,
+          plugins: [terserPluginCfg]
         },
         {
-            file: pkg.module,
-            format: 'es'
-        }
-    ],
+          file: `dist/index.cjs.js`,
+          format: 'cjs',
+          sourcemap: true,
+          plugins: [terserPluginCfg]
+        },
+      ],
     external: [
         ...Object.keys(pkg.dependencies || {})
     ],
     plugins: [
-        typescript({ typescript: require('typescript') }),
+        tsPluginConfig,
+        replacePluginCfg,
         postcss({
             inject: false,
             extract: false,
@@ -34,8 +70,5 @@ export default {
             ]
         }),
         html(),
-        terser({
-            mangle: false
-        })
-    ]
+    ].filter(Boolean)
 }
